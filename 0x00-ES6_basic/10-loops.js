1bb938a4bd4158d1ef1ef15aab258d169ddd86a0
WillingtonM <willington.mhlanga@gmail.com>
2024-01-04 03:26:16 +0200

    0x00. ES6 Basics

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -2,8 +2,8 @@
 /* eslint-disable no-param-reassign */
 export default function appendToEachArrayValue(array, appendString) {
   const my_array = [];
-  for (const item of array) {
-    my_array.push(appendString + item);
+  for (const i of array) {
+    my_array.push(appendString + i);
   }
 
   return my_array;
