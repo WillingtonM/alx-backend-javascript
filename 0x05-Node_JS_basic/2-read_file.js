e5d4c59d4d62d3d4f3178bcd6ca6e92aadfe536c
WillingtonM <willington.mhlanga@gmail.com>
2024-02-27 03:35:33 +0200

    0x05. NodeJS Basics

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,31 +1,46 @@
+#!/usr/bin/env node
+
+/* Ad-hoc script foor practicing nodejs */
+
 const fs = require('fs');
 
-module.exports = function countLearners(path) {
+function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, { encoding: 'utf-8' });
-    const datalines = data.split('\n').slice(1, -1);
-    const dataheader = data.split('\n').slice(0, 1)[0].split(',');
-    const idxField = dataheader.findIndex((e) => e === 'field');
-    const idxFirstName = dataheader.findIndex((e) => e === 'firstname');
-    const learners = {};
+    const lines = fs.readFileSync(path, { encoding: 'utf8' }).split(/\r?\n/);
+    let c = 0;
+    let learnerCount = 0;
     const fields = {};
 
-    datalines.forEach((line) => {
-      const dlist = line.split(',');
-      if (!fields[dlist[idxField]]) fields[dlist[idxField]] = 0;
-      fields[dlist[idxField]] += 1;
-      if (!learners[dlist[idxField]]) learners[dlist[idxField]] = '';
-      learners[dlist[idxField]] += learners[dlist[idxField]] ? `, ${dlist[idxFirstName]}` : dlist[idxFirstName];
-    });
-
-    console.log(`Number of learners: ${datalines.length}`);
-    for (const k in fields) {
-      if (Object.hasOwnProperty.call(fields, k)) {
-        const elem = fields[k];
-        console.log(`Number of learners in ${k}: ${elem}. List: ${learners[k]}`);
+    for (const l of lines) {
+      if (l.trim() !== '' && c > 0) {
+        learnerCount += 1;
+        const [fname, lname, age, field] = l.split(',');
+        if (!fields[field]) {
+          fields[field] = {
+            count: 1,
+            students: [fname],
+          };
+        } else {
+          const nCount = fields[field].count + 1;
+          const nLearner = (fields[field].students).concat(fname);
+          fields[field] = {
+            count: nCount,
+            students: nLearner,
+          };
+        }
       }
+      c += 1;
+    }
+
+    console.log(`Number of students: ${learnerCount}`);
+    for (const field of Object.keys(fields)) {
+      const n = fields[field].count;
+      const names = fields[field].students.join(', ');
+      console.log(`Number of students in ${field}: ${n}. List: ${names}`);
     }
   } catch (error) {
     throw new Error('Cannot load the database');
   }
-};
+}
+
+module.exports = countStudents;
