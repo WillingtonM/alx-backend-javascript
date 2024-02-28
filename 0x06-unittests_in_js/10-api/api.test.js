a17c79ff20f47e29fab2ea06e5c47a85950e6809
WillingtonM <willington.mhlanga@gmail.com>
2024-02-28 05:11:01 +0200

    0x06. Unittests in JS

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -2,113 +2,210 @@ const request = require("request");
 const {describe, it} = require("mocha");
 const expect = require("chai").expect;
 
-const API_URL = 'http://localhost:7865';
+const API_URL = '${API_URL}';
 
-describe("Index page", function() {
-    const optns = {
-		url: `${API_URL}/`,
-		method: "GET"
-    }
-    it("check correct status code", function(done) {
-		request(optns, function(er, resp, body) {
-			expect(resp.statusCode).to.equal(200);
-			done();
-		});
-    });
-    it("check correct content", function(done) {
-		request(optns, function(er, resp, body) {
-			expect(body).to.equal("Welcome to the payment system");
-			done();
-		});
-    });
-});
+describe('Integration Testing', () => {
+  describe('GET /', () => {
+    it('Code: 200 | Body: Welcome to the payment system', (done) => {
+      const optns = {
+        url: API_URL,
+        method: 'GET',
+      };
 
-describe("Cart page", function() {
-    it("check correct status code for correct url", function(done) {
-		request.get(`${API_URL}/cart/12`, function(er, resp, body) {
-			expect(resp.statusCode).to.equal(200);
-			done();
-		});
+      request(optns, function (er, resp, body) {
+        expect(resp.statusCode).to.equal(200);
+        expect(body).to.equal('Welcome to the payment system');
+        done();
+      });
     });
-    it("check correct content for correct url", function(done) {
-		request.get(`${API_URL}/cart/12`, function(er, resp, body) {
-			expect(body).to.equal("Payment methods for cart 12");
-			done();
-		});
-    });
-    it("check correct status code for incorrect url", function(done) {
-		request.get(`${API_URL}/cart/kim`, function(er, resp, body) {
-			expect(resp.statusCode).to.equal(404);
-			done();
-		});
-    });
-});
+  });
 
-describe("Available_payments page", function() {
-    it("check correct status for correct url", function() {
-		request.get(`${API_URL}/available_payments`, (er, resp, body) => {
-			if (er) {
-				expect(resp.statusCode).to.not.equal(200);
-			} else {
-				expect(resp.statusCode).to.equal(200);
-			}
-		});
-    });
+  describe('GET /cart/12', () => {
+    it('Responds with 200 and id 12 in msg', (done) => {
+      const optns = {
+        url: `${API_URL}/cart/12`,
+        method: 'GET',
+      };
 
-    it("check correct body content for correct url", function() {
-		const optn = {json: true};
-		const payLoad = {
-			payment_methods: {
-				credit_cards: true,
-				paypal: false
-			}
-		}
-		request.get(`${API_URL}/available_payments`, optn, (er, resp, body) => {
-			if (er) {
-				expect(resp.statusCode).to.not.equal(200);
-			} else {
-				expect(body).to.deep.equal(payLoad);
-			}
-		});
+      request(optns, function (er, resp, body) {
+        expect(resp.statusCode).to.equal(200);
+        expect(body).to.equal('Payment methods for cart 12');
+        done();
+      });
     });
-});
+  });
 
-describe("Login", function() {
-    it("check correct status code for request that's sent properly", function(done) {
-		const optn = {
-			url: `${API_URL}/login`,
-			json: true,
-			body: { userName: 'JOE' }
-		};
-		request.post(optn, function(er, resp, body) {
-			expect(resp.statusCode).to.equal(200);
-			done();
-		});
+  describe('GET /cart/1', () => {
+    it('Responds with 200 and id 1 in msg', (done) => {
+      const optns = {
+        url: `${API_URL}/cart/1`,
+        method: 'GET',
+      };
+
+      request(optns, function (er, resp, body) {
+        expect(resp.statusCode).to.equal(200);
+        expect(body).to.equal('Payment methods for cart 1');
+        done();
+      });
     });
-    it("check correct content for request that's sent properly", function(done) {
-		const opts = {
-			url: `${API_URL}/login`,
-			json: true,
-			body: { userName: 'JOE' }
-		};
-		request.post(opts, function(er, resp, body) {
-			if (er) {
-				expect(resp.statusCode).to.not.equal(200);
-			} else {
-				expect(body).to.contain('Welcome JOE');
-			}
-			done();
-		});
+  });
+
+  describe('GET /cart/123', () => {
+    it('Responds with 200 and id 12 in msg', (done) => {
+      const optns = {
+        url: `${API_URL}/cart/123`,
+        method: 'GET',
+      };
+
+      request(optns, function (er, resp, body) {
+        expect(resp.statusCode).to.equal(200);
+        expect(body).to.equal('Payment methods for cart 123');
+        done();
+      });
     });
-    it("check correct status code for request that's not sent properly", function(done) {
-		const optn = {
-			url: `${API_URL}/login`,
-			json: true,
-			body: { userName: 'JOE' }
-		};
-		request.post(optn, function(er, resp, body) {
-			expect(resp.statusCode).to.equal(404);
-			done();
-		});
+  });
+
+  describe('GET /cart/a12', () => {
+    it('Responds with 404', (done) => {
+      const optns = {
+        url: `${API_URL}/cart/a12`,
+        method: 'GET',
+      };
+
+      request(optns, function (er, resp, body) {
+        expect(resp.statusCode).to.equal(404);
+        done();
+      });
     });
+  });
+
+  describe('GET /cart/a12b', () => {
+    it('Responds with 404', (done) => {
+      const optns = {
+        url: `${API_URL}/cart/a12b`,
+        method: 'GET',
+      };
+
+      request(optns, function (er, resp, body) {
+        expect(resp.statusCode).to.equal(404);
+        done();
+      });
+    });
+  });
+
+  describe('GET /cart/12b', () => {
+    it('Responds with 404', (done) => {
+      const optns = {
+        url: `${API_URL}/cart/12b`,
+        method: 'GET',
+      };
+
+      request(optns, function (er, resp, body) {
+        expect(resp.statusCode).to.equal(404);
+        done();
+      });
+    });
+  });
+
+  describe('GET /cart/hello', () => {
+    it('Responds with 404', (done) => {
+      const optns = {
+        url: `${API_URL}/cart/hello`,
+        method: 'GET',
+      };
+
+      request(optns, function (er, resp, body) {
+        expect(resp.statusCode).to.equal(404);
+        done();
+      });
+    });
+  });
+
+  describe('GET /cart/', () => {
+    it('Responds with 404', (done) => {
+      const optns = {
+        url: `${API_URL}/cart/`,
+        method: 'GET',
+      };
+
+      request(optns, function (er, resp, body) {
+        expect(resp.statusCode).to.equal(404);
+        done();
+      });
+    });
+  });
+
+  describe('GET /available_payments JSON string', () => {
+    it('Responds with 200 and correct JSON string', (done) => {
+      const optns = {
+        url: `${API_URL}/available_payments`,
+        method: 'GET',
+      };
+
+      request(optns, function (er, resp, body) {
+        expect(resp.statusCode).to.equal(200);
+        expect(body).to.equal(
+          '{"payment_methods":{"credit_cards":true,"paypal":false}}'
+        );
+        done();
+      });
+    });
+  });
+
+  describe('GET /available_payments JSON parsed', () => {
+    it('Responds with 200 and correct JSON object when parsed', (done) => {
+      const optns = {
+        url: `${API_URL}/available_payments`,
+        method: 'GET',
+      };
+
+      request(optns, function (er, resp, body) {
+        expect(resp.statusCode).to.equal(200);
+        const bodyParsed = JSON.parse(body);
+
+        const referenceBody = {
+          payment_methods: {
+            credit_cards: true,
+            paypal: false,
+          },
+        };
+
+        expect(bodyParsed).to.deep.equal(referenceBody);
+        done();
+      });
+    });
+  });
+
+  describe('POST /login with body', () => {
+    it('Responds with 200 and correct name Betty', (done) => {
+      const optns = {
+        url: `${API_URL}/login`,
+        method: 'POST',
+        json: {
+          userName: 'Betty',
+        },
+      };
+
+      request(optns, function (er, resp, body) {
+        expect(resp.statusCode).to.equal(200);
+        expect(body).to.equal('Welcome Betty');
+        done();
+      });
+    });
+  });
+
+  describe('POST /login with no body', () => {
+    it('Responds with 200 and correct name Undefined', (done) => {
+      const optns = {
+        url: `${API_URL}/login`,
+        method: 'POST',
+      };
+
+      request(optns, function (er, resp, body) {
+        expect(resp.statusCode).to.equal(200);
+        expect(body).to.equal('Welcome undefined');
+        done();
+      });
+    });
+  });
 });
