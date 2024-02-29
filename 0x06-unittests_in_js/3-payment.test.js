fbd06ec0cd409ba693274dfbbcea1538594aa9bb
WillingtonM <willington.mhlanga@gmail.com>
2024-02-29 08:22:18 +0200

    0x06. Unittests in JS

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -2,19 +2,15 @@ const {describe, it} = require("mocha");
 const sinon = require("sinon");
 const sendPaymentRequestToApi = require("./3-payment");
 const Utils = require("./utils");
+const assert = require("assert");
 
 describe("sendPaymentRequestToApi", function() {
     it("check that Utils.calculateNumber was called once", function() {
-		const util_spy = sinon.spy(Utils, 'calculateNumber');
-		const log_spy = sinon.spy(console, 'log');
+	const util_spy = sinon.spy(Utils, "calculateNumber");
 
-		const api_request = sendPaymentRequestToApi(100, 20);
+	sendPaymentRequestToApi(50, 24.52);
 
-		expect(util_spy.calledOnceWithExactly('SUM', 100, 20)).to.equal(true);
-		expect(log_spy.calledWithExactly('The total is: 120')).to.equal(true);
-		expect(utils.calculateNumber('SUM', 100, 20)).to.equal(api_request);
-
-		util_spy.restore();
-		log_spy.restore();
+	assert(util_spy.calledOnce);
+	util_spy.restore();
     });
 });
